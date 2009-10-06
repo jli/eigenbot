89bea8c5032e1cdc8ff6100e64240bf6d982b7b1
John Li <jli@circularly.org>
2009-10-06 09:44:17 -0400

    throw away horrible GitHub date parsing

--- src/Plugins/Github.hs
+++ src/Plugins/Github.hs
@@ -15,7 +15,6 @@ import Control.Monad.State.Strict (get, put)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe (fromJust)
-import Data.List (splitAt)
 import Text.Printf (printf)
 
 import Text.Feed.Import (parseFeedString)
@@ -23,7 +22,7 @@ import Text.Feed.Types (Feed(..))
 import qualified Text.Atom.Feed as Atom
 
 import qualified Base as B
-import Util (appFst, delay, simpleHttp)
+import Util (delay, simpleHttp)
 
 type User = String
 type Project = String
@@ -35,46 +34,21 @@ data Repo = Repo User Project
 instance Show Repo where
     show (Repo u p) = printf "%s/%s" u p
 
-data SimpleTime = SimpleTime {
-      _stYear :: Int
-    , _stMonth :: Int
-    , _stDay :: Int
-    , _stHour :: Int
-    , _stMin :: Int
-    , _stSec :: Int
-} deriving (Eq)
-
-instance Show SimpleTime where
-    show (SimpleTime y mo d h m s) =
-        printf "%04d-%02d-%02d %02d:%02d:%02d" y mo d h m s
-
--- yuck
-dhms :: Int -> Int -> Int -> Int -> Int
-dhms d h m s = s + 60*m + 3600*h + 86400*d
-instance Ord SimpleTime where
-    compare (SimpleTime y1 mo1 d1 h1 m1 s1) (SimpleTime y2 mo2 d2 h2 m2 s2) =
-        case compare y1 y2 of
-          EQ ->
-            case compare mo1 mo2 of
-              EQ -> compare (dhms d1 h1 m1 s1) (dhms d2 h2 m2 s2)
-              other -> other
-          other -> other
-
+type RepoCommits = Map Repo [Commit]
 
 data Commit = Commit {
-      _cTitle :: String
+      cTitle :: String
     , _cAuthor :: String
-    , cTime :: SimpleTime
+    , _cTime :: String
 }
 
 instance Show Commit where
-    show (Commit title author time) =
-        printf "%s by %s %s" title author (show time)
+    show (Commit title _author _time) = title
 
 data GithubState = St {
       _ghSubscribed :: [Repo]
     , _ghJustStarted :: Bool -- FIXME should be per-channel
-    , _ghCommits :: Map Repo [Commit]
+    , _ghCommits :: RepoCommits
 }
 
 initState :: GithubState
@@ -102,7 +76,7 @@ loop _evq actq = do
     put $ St subscribed False newCommits
     liftIO $ delay 300 -- make configurable
 
-fetchAll :: [Repo] -> IO (Map Repo [Commit])
+fetchAll :: [Repo] -> IO (RepoCommits)
 fetchAll repos =
     foldM fetchAndAdd M.empty repos
   where fetchAndAdd commitMap repo = do
@@ -124,30 +98,17 @@ pageToCommits = map entryToCommit . Atom.feedEntries . fromJust . parseAtomStrin
 entryToCommit :: Atom.Entry -> Commit
 entryToCommit entry = Commit title author time
   where author = Atom.personName $ head $ Atom.entryAuthors entry
-        time = parseGithubTime $ Atom.entryUpdated entry
+        time = Atom.entryUpdated entry
         title = Atom.txtToString $ Atom.entryTitle entry
 
--- example: "2009-10-02T17:37:32-07:00"
--- FIXME really should get good at Parsec...
-parseGithubTime :: Atom.Date -> SimpleTime
-parseGithubTime ghTime =
-    SimpleTime y (mo) d h m s
-  where (y, t4) = appFst read $ splitAt 4 ghTime
-        (mo, t7) = appFst (toEnum . (+ 1) . read) $ splitAt 2 $ tail t4
-        (d, t10) = appFst read $ splitAt 2 $ tail t7
-        (h, t13) = appFst read $ splitAt 2 $ tail t10
-        (m, t16) = appFst read $ splitAt 2 $ tail t13
-        (s, _t19) = appFst read $ splitAt 2 $ tail t16
-
 -- FIXME dense and ugly
-announceNew :: B.ActQ -> Map Repo [Commit] -> Map Repo [Commit] -> IO ()
-announceNew actq newMap oldMap =
-    mapM_ announce newCommits
+announceNew :: B.ActQ -> RepoCommits -> RepoCommits -> IO ()
+announceNew actq newMap oldMap = mapM_ announce newCommits
   where newCommits = M.toList $ foldr removeOld newMap (M.toList oldMap)
         removeOld (repo, oldCommits) curMap =
-            let newestOld = cTime $ head oldCommits  -- be safer
+            let newestOld = cTitle $ head oldCommits  -- be safer
                 cur = fromJust $ M.lookup repo curMap
-                keepNew = takeWhile ((> newestOld) . cTime) cur
+                keepNew = takeWhile ((/= newestOld) . cTitle) cur
             in M.insert repo keepNew curMap
         announce (repo, newC) =
             forM_ (reverse newC)
