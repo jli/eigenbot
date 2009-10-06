7893ac1a3ef71b919b57744b1bc30501ccbc65c1
John Li <jli@circularly.org>
2009-10-05 23:46:55 -0400

    add random to Simple for more interesting messages

--- src/Plugins/Simple.hs
+++ src/Plugins/Simple.hs
@@ -1,47 +1,73 @@
 module Plugins.Simple (plugin) where
 
 import Control.Monad.Trans (liftIO)
-import Data.List (find, isPrefixOf)
-import Data.Maybe (isJust, maybe)
+import Control.Monad.State.Strict (get, put)
+import Data.List (find)
+import Data.Maybe (fromJust, isJust)
 import System.Time (getClockTime)
+import System.Random (StdGen, next, mkStdGen)
 import Text.Printf (printf)
 
 import qualified Base as B
-import Util (breakOnSpace)
+import Util (breakOnSpace, safeIndex)
 
--- This simple plugin has () as state.
+type RandState = StdGen
+
+initState :: RandState
+initState = mkStdGen 1337
 
 plugin :: B.Plugin
-plugin = B.genPlugin "simple plugin: !id, !tell, !date" loop ()
+plugin = B.genPlugin "simple plugin: !id, !tell, !date" loop initState
 
-loop :: B.PluginLoop ()
-loop evq actq = liftIO $ do
-    ev <- B.readEvent evq
+loop :: B.PluginLoop RandState
+loop evq actq = do
+    ev <- liftIO $ B.readEvent evq
     case ev of
-      B.ChannelMsg chan nick msg -> do
-        let (cmd, rest) = breakOnSpace msg
-            say = B.say actq chan
-        case cmd of
-          "!id" -> say $ printf "%s said:%s" nick rest
-          "!tell" ->
-            case words rest of
-              [] -> say $ printf "TRYING TO PICK A FIGHT %s YO!" nick
-              [user] -> say $ printf "hey %s, THE WORLD IS A VAMPIRE" user
-              user:rest' -> say $ printf "hey %s, %s" user $ unwords rest'
-          "!date" -> getClockTime >>= say . printf "date is: %s" . show
-          _ -> maybe (return ()) say (cuteness nick msg)
+      B.ChannelMsg chan nick msg -> handleChanMsg chan nick msg
       _ -> return ()
+  where handleChanMsg chan nick msg =
+          let (cmd, rest) = breakOnSpace msg
+              say = liftIO . B.say actq chan in
+          case cmd of
+            "!id" -> say $ printf "%s said:%s" nick rest
+            "!tell" ->
+              case words rest of
+                [] -> say $ printf "TRYING TO PICK A FIGHT %s YO!" nick
+                [user] -> say $ printf "hey %s, THE WORLD IS A VAMPIRE" user
+                user:rest' -> say $ printf "hey %s, %s" user $ unwords rest'
+            "!date" -> liftIO getClockTime >>= say . printf "date is: %s" . show
+            _ -> do
+              gen <- get
+              case cuteness nick msg gen of
+                Nothing -> return ()
+                Just (cuteMsg, gen') -> do
+                  say cuteMsg
+                  put gen'
 
--- FIXME some random would be cool
-cuteness :: B.Nick -> String -> Maybe String
-cuteness nick msg =
-    if addressedMe
-    then Just $ printf "hey %s ;) ;) ;)" nick
-    else if mentionedMe
-    then Just $ printf "YO WHAT"
-    else Nothing
-  where addressedMe =
-            case words msg of
-              first:_ -> B.me `isPrefixOf` first
-              [] -> False
-        mentionedMe = isJust $ find (== B.me) $ words msg
+-- FIXME bleh. must be nicer way to wrap up state access?
+cuteness :: B.Nick -> String -> StdGen -> Maybe (String, StdGen)
+cuteness nick msg gen =
+    if not mentionedMe
+    then Nothing
+    else Just msgAndNewGen
+  where mentionedMe = isJust $ find (== B.me) $ words msg
+        msgAndNewGen =
+            let (i, gen') = next gen
+                formatFn = fromJust $ safeIndex cuteFormats i
+            in (formatFn nick, gen')
+
+cuteFormats :: [B.Nick -> String]
+cuteFormats =
+    [ printf "hey %s ;) ;) ;)"
+    , const "YO WHAT"
+    , const "*hugs*"
+    , const "*kisses*"
+    , const "bitch if it ain't strongly typed then i ain't got the time"
+    , printf "%s mothafucka get that errorprone api outta my face"
+    , const "huff puff"
+    , printf "%s <3 <3 <3"
+    , const "I am tired of Earth, these people. I am tired of being caught in the tangle of their lives."
+    , const "dog carcass in alley this morning tire tread on burst stomach"
+    , printf "omg %s omg <3"
+    , const "(.).(.) :: (b -> c) -> (a -> a1 -> b) -> a -> a1 -> c"
+    ]
